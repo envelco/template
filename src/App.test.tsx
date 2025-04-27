a21933f28aab4479d097b83c26fdfb29e6ea83de
flornkm <florian.kiem@hfg.design>
2025-04-27 19:58:18 +0200

    feat: update package

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,9 @@
-import * as React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import * as React from "react"
+import { render } from "@testing-library/react"
+import App from "./App"
 
 test("does not render an learn angular link", () => {
-  const { queryByText } = render(<App />);
-  const linkElement = queryByText(/learn angular/i);
-  expect(linkElement).not.toBeInTheDocument();
-});
+  const { queryByText } = render(<App />)
+  const linkElement = queryByText(/learn angular/i)
+  expect(linkElement).not.toBeInTheDocument()
+})
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen text-center bg-white">
+    <div className="flex flex-col justify-center items-center min-h-screen text-center bg-white">
       Hello, Envel
     </div>
   )
